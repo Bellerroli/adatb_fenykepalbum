911780a8f7d1e6a9162ac7475523fc0398029b8e
Bellér Roland <h846438@stud.u-szeged.hu>
2024-04-26 09:27:22 +0200

    Képekhez tartoző kommentek SQL lekérdezése

--- src/main/java/hu/uszeged/fenykepalbum/repository/CommentRepository.java
+++ src/main/java/hu/uszeged/fenykepalbum/repository/CommentRepository.java
@@ -2,8 +2,15 @@ package hu.uszeged.fenykepalbum.repository;
 
 import hu.uszeged.fenykepalbum.model.CommentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<CommentModel, Long> {
+    @Query(value = "SELECT * FROM HOZZASZOLAS WHERE kep_id = :pictureid",
+    nativeQuery = true)
+    List<CommentModel> findCommentByPictureID(@Param("pictureid") long pictureID);
 }
--- src/main/java/hu/uszeged/fenykepalbum/service/CommentService.java
+++ src/main/java/hu/uszeged/fenykepalbum/service/CommentService.java
@@ -17,15 +17,8 @@ import java.util.List;
 public class CommentService {
     private final CommentRepository commentRepository;
 
-    //TODO: a következő mérföldkőre ezt a metódust át kell dolgozni
     public List<CommentModel> commentsByPictureiD(Long pictureID){
-        List<CommentModel> ret = new ArrayList<>();
-        //SQL query a CommentRepositoryban a foreach loop helyett
-        for(CommentModel cm: commentRepository.findAll()){
-            if(cm.getPictureID() == pictureID) ret.add(cm);
-        }
-
-        return ret;
+        return commentRepository.findCommentByPictureID(pictureID);
     }
 
     public CommentModel commentByID(int id){
